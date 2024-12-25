897fb6780dd58386cdb3f7ea272dab1543d887ef
TheR3dWizard <sreeraghavan2016@gmail.com>
2024-12-25 17:29:15 +0530

    added playback server

---
+++ server.py
@@ -0,0 +1,41 @@
+from flask import Flask, Response, send_file
+import threading
+
+app = Flask(__name__)
+global audio_data
+audio_data = None
+
+def generate_audio():
+    global audio_data
+    while True:
+        with open("Travelers.mp3", "rb") as audio_file:
+            data = audio_file.read(1024)
+            while data:
+                # print("Reading data") #WORKS
+                audio_data = data
+                data = audio_file.read(1024)
+
+
+@app.route('/stream')
+def stream_audio():
+    def generate():
+        while True:
+            with open("Travelers.mp3", "rb") as audio_file:
+                data = audio_file.read(1024)
+                while data:
+                    yield data
+                    data = audio_file.read(1024)
+    def generatelive():
+        global audio_data
+        while True:
+            if audio_data:
+                print("Sending data")
+                print(audio_data)
+                yield audio_data
+    return Response(generatelive(), mimetype="audio/mpeg")
+
+if __name__ == '__main__':
+    thread = threading.Thread(target=generate_audio)
+    thread.daemon = True
+    thread.start()
+    app.run(host='0.0.0.0', port=12345)
